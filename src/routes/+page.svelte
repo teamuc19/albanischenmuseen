06d88474eff2482019fccad573c39b4fa6560f0d
Tea <TeaMuc19@htl-shkoder.com>
2025-03-04 11:59:34 +0100

    New

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,2 @@
-<script>
-	import { onMount } from 'svelte';
-	import { fetchMuseums } from '$lib/api.js';
-
-	let museums = [];
-
-	onMount(async () => {
-		museums = await fetchMuseums();
-	});
-</script>
-
-<h1>Museums in Albania</h1>
-{#each museums as museum}
-	<div class="card">
-		<h3>{museum.name}</h3>
-		<p>Location: {museum.location}</p>
-		<p>Desc: {museum.description}</p>
-		<img src={museum.image_url} alt={museum.name} width="200" />
-	</div>
-{/each}
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="Api/Museen">svelte.dev/docs/kit</a> to read the documentation</p>
